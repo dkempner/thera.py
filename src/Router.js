f535abde55b35162342ee28d6077bb8585d19873
Daniel Kempner <djkempner@gmail.com>
2018-11-18 12:13:29 -0800

    persistent nav

--- src/Router.js
+++ src/Router.js
@@ -1,20 +1,61 @@
-import React, { Component } from 'react';
-import Home from './Home';
-import Nav from './Nav';
-import Personas from './Personas';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import Home from "./Home";
+import Personas from "./Personas";
+import { BrowserRouter, Route } from "react-router-dom";
+import "./index.css";
 
 class Router extends Component {
-    render() {
-        return (
-            <BrowserRouter>
-                <div>
-                    <Nav></Nav>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route path="/personas" component={Personas}></Route>
-                </div>
-            </BrowserRouter>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.routes = [
+      {
+        path: "/",
+        component: () => <Home />,
+        exact: true
+      },
+      {
+        path: "/personas",
+        component: () => <Personas />
+      }
+    ];
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <div id="left" className="column">
+            <div className="" id="top-left">
+              My App
+            </div>
+            <div className="bottom">
+              <ul className="list-unstyled">
+                <li>Home</li>
+                <li>Projects</li>
+                <li>Users</li>
+              </ul>
+            </div>
+          </div>
+          <div id="right" className="column">
+            <div id="top-right">
+              Other App-y stuff
+            </div>
+            <div className="bottom">
+              <div>
+                {this.routes.map(route => (
+                  <Route
+                    key={route.path}
+                    path={route.path}
+                    exact={route.exact}
+                    component={route.component}
+                  />
+                ))}
+              </div>
+            </div>
+          </div>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
 }
 export default Router;
