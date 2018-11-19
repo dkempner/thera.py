0c364470a6af64b307a5d1bed5c328ae2a29004f
Daniel Kempner <djkempner@gmail.com>
2018-11-18 20:37:24 -0800

    hygiene

--- src/store.js
+++ src/store.js
@@ -1,14 +1,19 @@
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
-import rootReducer from './reducers';
+import rootReducer from "./reducers";
 
 const initState = {
-  therapist: {
-    name: ''
-  }
+  therapist: {}
 };
 
 const middleware = [thunk];
-const store = createStore(rootReducer, initState, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer,
+  initState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 export default store;
